5c1126ee5482b504cd0973cd10ffc6ba7abb9046
Your Name <you@example.com>
2021-10-12 21:25:42 +0000

    static website

--- static_website.tf
+++ static_website.tf
@@ -1,5 +1,5 @@
 module "static-site" {
-  source            = "git@github.com:telia-oss/terra?ref=31db0be0ccc25de0750557230560d4be7c96779a"
+  source            = "github.com:telia-oss/terra?ref=31db0be0ccc25de0750557230560d4be7c96779a"
   bucket_versioning = false
   hosted_zone_name  = "thecloudcollege.com"
   name_prefix       = "glenn"
