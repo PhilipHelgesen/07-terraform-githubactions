b57c8dc1dec403d4ae34a6de77ff95fe69f560ae
Your Name <you@example.com>
2021-10-12 21:19:27 +0000

    module source

--- static_website.tf
+++ static_website.tf
@@ -1,5 +1,5 @@
 module "static-site" {
-  source            = "telia-oss/static-site/aws?ref=31db0be0ccc25de0750557230560d4be7c96779a"
+  source            = "git@github.com:telia-oss/terra?ref=31db0be0ccc25de0750557230560d4be7c96779a"
   version           = "3.0.0"
   bucket_versioning = false
   hosted_zone_name  = "thecloudcollege.com"
