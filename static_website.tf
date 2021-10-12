4dec7417ba50aee83914d57087bdf0b95a4fe921
Your Name <you@example.com>
2021-10-12 21:22:44 +0000

    prefictable name of pipeline

--- static_website.tf
+++ static_website.tf
@@ -1,6 +1,5 @@
 module "static-site" {
   source            = "git@github.com:telia-oss/terra?ref=31db0be0ccc25de0750557230560d4be7c96779a"
-  version           = "3.0.0"
   bucket_versioning = false
   hosted_zone_name  = "thecloudcollege.com"
   name_prefix       = "glenn"
