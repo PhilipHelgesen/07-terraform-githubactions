6158c57415926b51b06929f747984717e7d1894e
Your Name <you@example.com>
2021-10-12 21:30:11 +0000

    static website

--- static_website.tf
+++ static_website.tf
@@ -1,6 +1,5 @@
 module "static-site" {
   source            = "github.com/telia-oss/terraform-aws-static-site?ref=31db0be0ccc25de0750557230560d4be7c96779a"
-  version           = "3.0.0"
   bucket_versioning = true
   hosted_zone_name  = "thecloudcollege.com"
   name_prefix       = "glenn"
