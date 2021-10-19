3b6b9c9ccf412d8b5ba0d043d38a1243cfd4695b
Philip Helgesen <55789033+PhilipHelgesen@users.noreply.github.com>
2021-10-19 09:55:39 +0200

    Update static_website.tf

--- static_website.tf
+++ static_website.tf
@@ -3,7 +3,7 @@ module "static-site" {
   version           = "3.1.0"
   bucket_versioning = false
   hosted_zone_name  = "thecloudcollege.com"
-  name_prefix       = "glenn"
-  site_name         = "glenn.thecloudcollege.com"
-  bucket_name       = "bechgle.thecloudcollege.com"
+  name_prefix = "name_prefix"  
+  site_name = "site_name"    
+  bucket_name = "bucket_name"
 }
